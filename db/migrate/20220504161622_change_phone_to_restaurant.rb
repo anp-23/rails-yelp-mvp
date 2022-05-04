54d9a82365d4a86bfab8b316247e1debde68a7c4
Alan Perkul <alanperkul@gmail.com>
2022-05-04 16:49:59 -0300

    2ex

---
+++ db/migrate/20220504161622_change_phone_to_restaurant.rb
@@ -0,0 +1,5 @@
+class ChangePhoneToRestaurant < ActiveRecord::Migration[6.1]
+  def change
+    change_column :restaurants, :phone_number, :string
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+x0 = {name: "Star Wars", address: "Siempreviva 123", phone_number: 55555, category: "chinese"}
+x1 = {name: "Sar Warst", address: "asfsgasg3", phone_number: 212525, category: "italian"}
+x2 = {name: "Stasrs", address: "151saf 123", phone_number: 5521532662555, category: "french"}
+x3 = {name: "Sagejrhrs", address: "12safaga 123", phone_number: 2122224124, category: "belgian"}
+x4 = {name: "S124faars", address: "22dafasfafas 123", phone_number: 5555151515, category: "japanese"}
+
+[x0, x1, x2, x3, x4].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+    puts "Created #{restaurant.name}"
+  end
+puts "Finished!"
